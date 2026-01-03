2a0baa40ef0026b13604bca625ac85d5b663a258
Serge Goussev <serge.goussev@statcan.gc.ca>
2026-01-03 00:21:15 -0500

    Get most of #4 working: - multilingual approach works - basic redirect to english set up - dynamic toggling on language

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Personal site
+
+To render locally, run:
+
+```bash
+preview-gh.sh
+
+Then open your browser to http://localhost:8000
+```
--- preview-gh.sh
+++ preview-gh.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
+## Script to preview the site locally - which is difficult for 
+## multilingual sites as there is multiple renders
 
 echo "🧹 Cleaning up old builds..."
 rm -rf _site
-
-echo "Rendering root redirect index.html and move it to _site/ ..."
-quarto render index.qmd --to html
-cp index.html _site/index.html
 
 echo "Rendering English..."
 quarto render --profile en
@@ -13,8 +11,11 @@ quarto render --profile en
 echo "Rendering French..."
 quarto render --profile fr
 
+echo "render Root... "
+quarto render redirect.qmd -o index.html
+
 echo "✅ Build Complete!"
-echo "🚀 Serving site at http://localhost:8080/"
+echo "🚀 Serving site at http://localhost:8000/"
 echo "   (Press Ctrl+C to stop)"
 
 # Serve from the PARENT of the repo folder to simulate the path
